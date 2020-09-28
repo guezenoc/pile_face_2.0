5289c7878cfc694eb0d2116a13b51e5b11f6e892
guezenoc <guezenoc.axel@gmail.com>
2020-09-28 16:17:22 +0200

    étape 9

--- pileFace.js
+++ pileFace.js
@@ -3,28 +3,6 @@
         **/
 
 
-//pile ou face 
-function pileouface() {
-    let a = Math.random();
-    console.log(a);
-
-    let b = Math.round(a);
-    return b; // porte de sortie
-}
-
-function lancer(porteDentre) {
-
-    switch (porteDentre) {
-        case 1:
-            console.log("face");
-            break;
-        case 0:
-            console.log("pile");
-            break;
-    }
-}
-
-lancer(pileouface());
 
 /**Créez une fonction principale "main()"
  *  qui sera la fonction lancée par votre code et appellera toutes les autres. 
