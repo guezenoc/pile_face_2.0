67efd24e6d7ccd46d653b496e8119d50a3cda2bc
guezenoc <guezenoc.axel@gmail.com>
2020-09-28 15:13:44 +0200

    étape6

--- pileFace.js
+++ pileFace.js
@@ -1,6 +1,7 @@
 /** Fonction  
         * @author Axel 
         **/
+    
 
 //pile ou face 
 function pileouface(){
@@ -24,3 +25,35 @@ function lancer(porteDentre) {
 }
  
 lancer (pileouface());
+
+/**Créez une fonction principale "main()"
+ *  qui sera la fonction lancée par votre code et appellera toutes les autres. 
+ * Cette fonction renvoie "vrai" sauf en cas d'erreur. 
+ */
+function main() {
+    return ""
+}
+/**déclaration variable globale "pseudo"*/
+let pseudo=("");
+
+/** fonction qui demande le pseudo à l'utilisateur */
+function demandePseudo() {
+    let pseudo = prompt("choisi ton pseudo")
+    return pseudo
+}
+
+/**création function compteVoyelles(mot) */
+function compteVoyelles(mot){
+
+    let tabVoyelles = ['a','e','i','o','u','y'];
+
+    for (let i = 0; i < mot.length; i++) {       
+        for (let y = 0; y < tabVoyelles.length; y++) {
+            if (mot[i]=== tabVoyelles[y]) {
+                
+            }
+            
+        }
+    } 
+}
+    
